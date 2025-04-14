78d2932945dcaa5ef54cff11ceeee4e1364065da
Riiiiiiiiiiick <drw.ricks.0@gmail.com>
2025-04-14 15:07:28 +0200

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+    background(0);
+}
+
+function draw() {
+   translate(width/2,height/2)
+
+   const d=map(sin(frameCount*0.051), -1,1,20,150)
+   const x= sin(frameCount*0.009)*width*0.3
+   const y= sin(frameCount*0.012+0.2)*height*0.3
+
+   noStroke();
+
+   const r= map(sin(frameCount*0.051), -1,1,0,255)
+   const b= map(cos(frameCount*0.047), -1,1,0,255)
+   const g= map(sin(frameCount*0.072), -1,1,0,255)
+
+   fill(r,g,b);
+
+   ellipse(x,y,d);
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth,windowHeight)
+    background(0);
+}
