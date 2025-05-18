8d9787d248e829cea6b9fc23e8fdcdc3756d2b77
Riiiiiiiiiiick <drw.ricks.0@gmail.com>
2025-05-18 15:30:06 +0200

    Create lab.js

---
+++ lab.js
@@ -0,0 +1,31 @@
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+  background(10, 10, 10);
+  pixelDensity(4)
+
+  stroke(192,192,192);
+  noFill();
+  strokeWeight(0.75);
+  line(width/2,33,width/2,height-33);
+
+  noStroke();
+  fill(192,192,192);
+
+  strokeJoin(ROUND);
+  textSize(52.8);
+  textLeading(45);
+  textStyle(BOLD);
+  textFont("Helvetica");
+  text('REACTION /\nDIFFUSION',width/2+12,height/2);
+
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(10, 10, 10);
+}
+
