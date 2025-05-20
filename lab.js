b0d1b81d0dd1049e431ffe5e18cebe0d25edf96f
Riiiiiiiiiiick <drw.ricks.0@gmail.com>
2025-05-20 07:29:05 +0200

    Update lab.js

--- lab.js
+++ lab.js
@@ -6,13 +6,15 @@ function draw(){
   background(10, 10, 10);
   pixelDensity(4)
 
+  reaction();
+
+}
+
+function reaction(){
   stroke(192,192,192);
   noFill();
   strokeWeight(0.75);
   line(width/2,33,width/2,height-33);
-
-  noStroke();
-  fill(192,192,192);
 
   strokeJoin(ROUND);
   textSize(52.8);
@@ -20,7 +22,6 @@ function draw(){
   textStyle(BOLD);
   textFont("Helvetica");
   text('REACTION /\nDIFFUSION',width/2+12,height/2);
-
 }
 
 
